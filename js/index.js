4a4fe26d8f23a85f5c583b9d05afc9f481576933
Kavi Patel <37132833+kavi678@users.noreply.github.com>
2018-05-17 04:08:11 +0930

    Update index.js

--- js/index.js
+++ js/index.js
@@ -34,12 +34,12 @@ popupWindow = window.open(url,'popUpWindow','height=300,width=700,left=50,top=50
 
 function openNav() {
       document.getElementById("sidebar").style.width = "250px";
-      document.getElementById("main").style.marginLeft = "250px";
+     
       document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
 
 function closeNav() {
       document.getElementById("sidebar").style.width = "0";
-      document.getElementById("main").style.marginLeft= "0";
+      
       document.body.style.backgroundColor = "white";
   }
