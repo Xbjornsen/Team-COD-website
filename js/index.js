7457a6d023ef3f79815893f8ced5c6f5e02c2497
Kavi Patel <37132833+kavi678@users.noreply.github.com>
2018-05-17 18:04:33 +0930

    Update index.js

--- js/index.js
+++ js/index.js
@@ -28,18 +28,56 @@ $(document).ready(function() {
 
 })
 
-function basicPopup(url) {
-popupWindow = window.open(url,'popUpWindow','height=300,width=700,left=50,top=50,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
-	}
+
 
 function openNav() {
       document.getElementById("sidebar").style.width = "250px";
-     
       document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
 
 function closeNav() {
       document.getElementById("sidebar").style.width = "0";
-      
       document.body.style.backgroundColor = "white";
+  }
+
+
+
+  var log = document.getElementById('log');
+  var btn = document.getElementById("signin");
+  var btna = document.getElementById("signi");
+  var span1 = document.getElementsByClassName("close1")[0];
+  btn.onclick = function() {
+      log.style.display = "block";
+  }
+  btna.onclick = function() {
+      log.style.display = "block";
+  }
+  span1.onclick = function() {
+      log.style.display = "none";
+  }
+
+
+
+  var sign = document.getElementById('sign');
+  var btn1 = document.getElementById("signon");
+  var btn1a = document.getElementById("signo");
+  var span2 = document.getElementsByClassName("close2")[0];
+  btn1.onclick = function() {
+      sign.style.display = "block";
+  }
+  btn1a.onclick = function() {
+      sign.style.display = "block";
+  }
+  span2.onclick = function() {
+      sign.style.display = "none";
+
+  }
+
+  window.onclick = function(event) {
+      if (event.target == sign) {
+          sign.style.display = "none";
+      }
+      if (event.target == log) {
+        log.style.display = "none";
+      }
   }
